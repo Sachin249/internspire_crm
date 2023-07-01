e4902c8255acfaa4a3c0b54d4aed5e7e27029d2a
Sachin <sachinsen249@gmail.com>
2023-07-01 21:57:33 +0530

    01/07/2023

--- controllers/user/profilecontroller.js
+++ controllers/user/profilecontroller.js
@@ -7,7 +7,7 @@ var verifyToken = require('../../middleware/verifytokenuser');
 
 router.get('/getprofile',verifyToken, async function(req, res, next){
     try{
-            const data = await users.find({"_id":req.decoded.id,"is_admin":0}).exec();
+            const data = await users.find({"_id":req.decoded.id}).exec();
             return res.status(200).json({ success:'Data found', data:data });
     }catch(err){
         return res.status(500).json({ errors: err });
